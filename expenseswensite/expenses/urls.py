7b5ecbe10422353225668f1b95e103dab9f7ee52
selcanatak2629 <selcanatak2629@gmail.com>
2022-12-04 01:51:26 +0300

    refactor

--- expenseswensite/expenses/urls.py
+++ expenseswensite/expenses/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 
     path('', views.index, name="expenses"),
-    path('add-expense', views.add_expense, name="add-expenses")
-    
+    path('add-expense', views.add_expense, name="add-expense")
+
 ]
--- expenseswensite/expenses/views.py
+++ expenseswensite/expenses/views.py
@@ -6,5 +6,5 @@ def index(request):
     return render(request, 'expenses/index.html')
 
 def add_expense(request):
-    return render(request, 'expenses/add-expense.html')
+    return render(request, 'expenses/add_expense.html')
     
